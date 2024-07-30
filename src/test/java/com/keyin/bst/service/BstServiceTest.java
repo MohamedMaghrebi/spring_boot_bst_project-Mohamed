d4588dc5fe00f1b2bd754e90925f362ce43ca217
MohamedMaghrebi <138633077+MohamedMaghrebi@users.noreply.github.com>
2024-07-30 12:45:21 -0230

    add files

--- src/test/java/com/keyin/bst/service/BstServiceTest.java
+++ src/test/java/com/keyin/bst/service/BstServiceTest.java
@@ -15,7 +15,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 
-@SpringBootTest
+//@SpringBootTest
 public class BstServiceTest {
 
     @Autowired
